e828160bf1b6ce1b3561dcca944241f423dd3110
jay graves <jaywgraves@gmail.com>
2024-12-23 18:27:33 -0600

    add new strategy 'jerk'

--- knucklebones.py
+++ knucklebones.py
@@ -203,3 +203,33 @@ def first_available(die, a, b):
         return desc, None
     # find first open column on my board and put the die in it
     return desc, a.board.avail_columns()[0]
+
+def jerk(die, a, b):
+    # 'a' and 'b' can refer to either p1 or p2 based
+    # on who is playing this turn
+    desc = 'jerk'
+    if die is None:   # getting description w/o advancing random seed
+        return desc, None
+    # using my available columns, figure out which opponent's column
+    # has the most of my die
+    opp_cols = b.board._columns()
+    my_avail = a.board.avail_columns()
+    max_cnt = 0
+    max_cnt_col = []
+    for c in my_avail:
+        cnt = opp_cols[c].count(die)
+        if not max_cnt:
+            max_cnt = cnt
+            max_cnt_col.append(c)
+        else:
+            if cnt == max_cnt:
+                max_cnt_col.append(c)
+            elif cnt > max_cnt:
+                max_cnt = cnt
+                max_cnt_col.clear()
+                max_cnt_col.append(c)
+    if max_cnt == 0:
+        choice = a.choose(my_avail)
+    else:
+        choice = a.choose(max_cnt_col)
+    return desc, choice
