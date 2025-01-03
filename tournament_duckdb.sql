9e32f53677f7796f72c20890be8300c2c561d114
jay graves <jaywgraves@gmail.com>
2025-01-03 16:42:49 -0600

    add more columns to derived 'games' table

--- tournament_duckdb.sql
+++ tournament_duckdb.sql
@@ -18,12 +18,30 @@ p2 as (select
     strat,
     player_seed
 from turns
-where round_cnt = 1 and turn_cnt = 2)
+where round_cnt = 1 and turn_cnt = 2),
+final as (select
+    game_nbr,
+    result,
+    p1_score,
+    p1_cnt,
+    p2_score,
+    p2_cnt,
+    abs(p1_score - p2_score) as delta
+    from turns
+    where result is not null
+)
 select
     p1.game_nbr,
     p1.strat as p1_strat,
     p1.player_seed as p1_seed,
+    final.p1_score,
+    final.p1_cnt,
     p2.strat as p2_strat,
-    p2.player_seed as p2_seed
-from p1 inner join p2 on (p1.game_nbr = p2.game_nbr)
+    p2.player_seed as p2_seed,
+    final.p2_score,
+    final.p2_cnt,
+    final.delta
+from p1
+inner join p2 on (p1.game_nbr = p2.game_nbr)
+join final on (p1.game_nbr = final.game_nbr)
 order by p1.game_nbr;
